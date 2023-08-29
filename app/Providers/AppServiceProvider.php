95c9bf2bd36754f4cf25f65f5b60b6212e40af26
やなぎさわ <y.y.baske.74c@gmail.com>
2023-08-29 14:51:48 +0900

    URLをhttpsに変更

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,7 +1,8 @@
 <?php
 
 namespace App\Providers;
-
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +20,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        if (App::environment('production','staging')) {
+            URL::forceScheme('https');
+        }
     }
 }
